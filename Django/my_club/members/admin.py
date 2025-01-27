c95dccb58063cf5f1b556d5fec69193a54a8b3e7
sajjad.aemmi <sajjad.aemmi@partsoftware.com>
2025-01-27 09:16:53 +0330

    Add tflite example

--- Django/my_club/members/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Member
-
-class MemberAdmin(admin.ModelAdmin):
-    list_display = ("firstname", "lastname", "joined_date",)
-  
-admin.site.register(Member, MemberAdmin)
